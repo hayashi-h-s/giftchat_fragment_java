ad77036a5980089656966880cdf877987f26a364
hayamizu_shinji <hayamizu.hnut@gmail.com>
2020-08-26 12:20:40 +0900

    add

--- app/src/main/java/com/haya/giftchat_fragment_java/MainActivity.java
+++ app/src/main/java/com/haya/giftchat_fragment_java/MainActivity.java
@@ -1,6 +1,7 @@
 package com.haya.giftchat_fragment_java;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
 import androidx.fragment.app.FragmentTransaction;
 
 import android.os.Bundle;
@@ -18,6 +19,8 @@ public class MainActivity extends AppCompatActivity {
 
     button1 = findViewById(R.id.button1);
 
+    Fragment container = findViewById(R.id.container);
+
 //     コードからFragmentを追加
 //
 //     Fragmentを作成します
@@ -25,19 +28,21 @@ public class MainActivity extends AppCompatActivity {
     button1.setOnClickListener(new View.OnClickListener() {
       @Override
       public void onClick(View view) {
-        MainFragment fragment = new MainFragment();
+//        MainFragment fragment = new MainFragment();
 
-////     Fragmentの追加や削除といった変更を行う際は、Transactionを利用します
-////    // 新しく追加を行うのでaddを使用します
-////    // 他にも、よく使う操作で、replace removeといったメソッドがあります
-////    // メソッドの1つ目の引数は対象のViewGroupのID、2つ目の引数は追加するfragment
+        Fragment container = findViewById(R.id.container);
+//
+//////     Fragmentの追加や削除といった変更を行う際は、Transactionを利用します
+//////    // 新しく追加を行うのでaddを使用します
+//////    // 他にも、よく使う操作で、replace removeといったメソッドがあります
+//////    // メソッドの1つ目の引数は対象のViewGroupのID、2つ目の引数は追加するfragment
         FragmentTransaction transaction = getSupportFragmentManager().beginTransaction();
-
-        transaction.remove(R.id.container, fragment).commit();
-
-//    transaction.add(R.id.container, fragment);
-////     最後にcommitを使用することで変更を反映します
-        transaction.commit();
+//
+        transaction.remove(container).commit();
+//
+////    transaction.add(R.id.container, fragment);
+//////     最後にcommitを使用することで変更を反映します
+//        transaction.commit();
       }
     });
 
